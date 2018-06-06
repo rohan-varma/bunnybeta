c35976d63723141250e46c16a74cd739cce0a787
Yuchuan <yuan.yuchuan@affirm.com>
2018-06-06 14:24:04 -0700

    add more aliases

--- keywords.py
+++ keywords.py
@@ -4,7 +4,11 @@ aliased_commands = {
     "namely": "https://affirm.namely.com/",
     "lunch": "https://affirm.onelogin.com/client/apps/select/182706142",
     "dinner": "https://affirm.onelogin.com/client/apps/select/182706131",
-    "expensify": "https://affirm.onelogin.com/client/apps/select/182706120"
+    "expensify": "https://affirm.onelogin.com/client/apps/select/182706120",
+    "grafana": "https://grafana.affirm-keyhole.com/",
+    "jira": "https://jira.team.affirm.com/secure/Dashboard.jspa",
+    "help": "/help"
+
 }
 
 repos = {
